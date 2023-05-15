fe87c39913c5665812b07cacfb045f64198b2acf
THANH290803 <92362723+THANH290803@users.noreply.github.com>
2023-05-15 20:16:24 +0700

    Add not database

---
+++ addThem/addThem/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace addThem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMembers = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbLast = new System.Windows.Forms.TextBox();
+            this.tbFirst = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMembers
+            // 
+            this.dgvMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMembers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2, this.Column3, this.Column4, this.Column5 });
+            this.dgvMembers.Location = new System.Drawing.Point(22, 400);
+            this.dgvMembers.Name = "dgvMembers";
+            this.dgvMembers.RowTemplate.Height = 24;
+            this.dgvMembers.Size = new System.Drawing.Size(762, 284);
+            this.dgvMembers.TabIndex = 49;
+            this.dgvMembers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMembers_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "FirstName";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "LastName";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Address";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Phone";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Age";
+            this.Column5.Name = "Column5";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(230, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(330, 48);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Member";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(56, 326);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(136, 30);
+            this.btnAdd.TabIndex = 47;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(130, 251);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(205, 22);
+            this.tbAge.TabIndex = 46;
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(498, 167);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(227, 22);
+            this.tbPhone.TabIndex = 45;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(130, 166);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(205, 22);
+            this.tbAddress.TabIndex = 44;
+            // 
+            // tbLast
+            // 
+            this.tbLast.Location = new System.Drawing.Point(498, 102);
+            this.tbLast.Name = "tbLast";
+            this.tbLast.Size = new System.Drawing.Size(227, 22);
+            this.tbLast.TabIndex = 43;
+            // 
+            // tbFirst
+            // 
+            this.tbFirst.Location = new System.Drawing.Point(130, 102);
+            this.tbFirst.Name = "tbFirst";
+            this.tbFirst.Size = new System.Drawing.Size(205, 22);
+            this.tbFirst.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 23);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Age";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(385, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 23);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Phone";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 23);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Address";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(385, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Lastname";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(17, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 22);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Frirstname";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 713);
+            this.Controls.Add(this.dgvMembers);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbLast);
+            this.Controls.Add(this.tbFirst);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.DataGridView dgvMembers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbLast;
+        private System.Windows.Forms.TextBox tbFirst;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ addThem/addThem/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace addThem
+{
+    public partial class Form1 : Form
+    {
+        private List<Member> _members = new List<Member>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string memberFirstName = tbFirst.Text,
+                memberLastName = tbLast.Text,
+                memberAddress = tbAddress.Text,
+                memberPhone = tbPhone.Text;
+            int memberAge;
+            if (!string.IsNullOrWhiteSpace(memberFirstName) && !string.IsNullOrWhiteSpace(memberLastName) && !string.IsNullOrWhiteSpace(memberAddress) && !string.IsNullOrWhiteSpace(memberPhone) && int.TryParse(tbAge.Text, out memberAge))
+            {
+                Member member = new Member(memberFirstName, memberLastName, memberAddress, memberPhone, memberAge);
+                _members.Add(member);
+                RefreshMemberList();
+
+                tbFirst.Text = string.Empty;
+                tbLast.Text = string.Empty;
+                tbAddress.Text = string.Empty;
+                tbPhone.Text = string.Empty;
+                tbPhone.Text = string.Empty;
+
+                MessageBox.Show("Them thanh cong");
+
+            }
+            else
+            {
+                MessageBox.Show("That bai");
+            }
+        }
+
+        private void dgvMembers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            RefreshMemberList();
+        }
+        
+        private void RefreshMemberList()
+        {
+            dgvMembers.Rows.Clear();
+            foreach (var member in _members)
+            {
+                dgvMembers.Rows.Add(member.firstName, member.LastName, member.address, member.phone, member.Age);
+            }
+        }
+        
+        
+    }
+}
+
+public class Member
+{
+    public string firstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string address { get; set; }
+    
+    public string phone { get; set; }
+    public int Age { get; set; }
+
+
+    public Member(string firstName, string lastName, string address, string phone, int age)
+    {
+        this.firstName = firstName;
+        LastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        Age = age;
+    }
+}
